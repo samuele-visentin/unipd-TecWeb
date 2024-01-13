5f22d33cae0df1ed522e772a7cae4f07c68cf4c2
Andrea Mangolini <andrea.mangolini4@gmail.com>
2024-01-13 17:09:50 +0100

    Refactoring html/css

---
+++ src/cases.php
@@ -0,0 +1,27 @@
+<?php
+    $layout = file_get_contents("templates/layout.html");
+
+    $title = 'I nostri casi | Clue Catchers';
+    $keywords = '';
+    $description = '';
+    $breadcrumbs = '<p><a href="index.php" lang="en">Home</a> &raquo; I nostri casi</p>';
+    $content = '';
+    $menu = '
+        <p class="menuSection">Generale</p>
+        <ul>
+            <li id="menuButton-home"><a href="index.php" lang="en">Home</a></li>
+            <li class="menuSelected" id="menuButton-cases">I nostri casi</li>
+            <li id="menuButton-about"><a href="about.html">Chi siamo</a></li>
+            <li id="menuButton-faq"><a href="faq.html" lang="en"><abbr title="Frequently Asked Questions">FAQ</abbr></a></li>
+        </ul>
+    ';
+
+    $page = str_replace("[title]", $title, $layout);
+    $page = str_replace("[keywords]", $keywords, $page);
+    $page = str_replace("[description]", $description, $page);
+    $page = str_replace("[breadcrumbs]", $breadcrumbs, $page);
+    $page = str_replace("[content]", $content, $page);
+    $page = str_replace("[menu]", $menu, $page);
+
+    echo $page;
+?>
---
+++ src/index.php
@@ -0,0 +1,27 @@
+<?php
+    $layout = file_get_contents("templates/layout.html");
+
+    $title = 'Clue Catchers';
+    $keywords = '';
+    $description = '';
+    $breadcrumbs = '<p><span lang="en">Home</span></p>';
+    $content = '<h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1><h1>ciao</h1>';
+    $menu = '
+        <p class="menuSection">Generale</p>
+        <ul>
+            <li class="menuSelected" id="menuButton-home"><span lang="en">Home</span></li>
+            <li id="menuButton-cases"><a href="cases.php">I nostri casi</a></li>
+            <li id="menuButton-about"><a href="about.html">Chi siamo</a></li>
+            <li id="menuButton-faq"><a href="faq.html" lang="en"><abbr title="Frequently Asked Questions">FAQ</abbr></a></li>
+        </ul>
+    ';
+
+    $page = str_replace("[title]", $title, $layout);
+    $page = str_replace("[keywords]", $keywords, $page);
+    $page = str_replace("[description]", $description, $page);
+    $page = str_replace("[breadcrumbs]", $breadcrumbs, $page);
+    $page = str_replace("[content]", $content, $page);
+    $page = str_replace("[menu]", $menu, $page);
+
+    echo $page;
+?>
