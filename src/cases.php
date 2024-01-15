cddac800e2eaf869b2a3230fa3f7dcfe6ff50de9
Andrea Mangolini <andrea.mangolini4@gmail.com>
2024-01-15 19:30:46 +0100

    Impostazione iniziale bacheca senza link

--- src/cases.php
+++ src/cases.php
@@ -1,13 +1,39 @@
 <?php
     require_once("app/global.php");
+    require_once("data/indagine.php");
+
+    /***********************************
+        impostazione layout CASE
+    ***********************************/
+    $caseLayout = file_get_contents("templates/case_layout.html");
+    $caseArray = array();
+    $cases = getAllIndagine();
+    
+    foreach($cases as $case) {
+        $caseTitle = $case->nome;
+        $caseImgPath = isset($case->image_path) ? ($case->image_path) : 'assets/images/img_placeholder.png';
+        $caseDescription = $case->descrizione;
+
+        $newCase = str_replace("[caseTitle]", $caseTitle, $caseLayout);
+        $newCase = str_replace("[caseImgPath]", $caseImgPath, $newCase);
+        $newCase = str_replace("[caseDescription]", $caseDescription, $newCase);
+        $caseArray[] = $newCase;
+    }
+
+    /***********************************
+        impostazione layout PAGE
+    ***********************************/
 
     $layout = file_get_contents("templates/layout.html");
 
     $title = 'I nostri casi | Clue Catchers';
     $keywords = '';
     $description = '';
-    $breadcrumbs = '<p><a href="index.php" lang="en">Home</a> &raquo; I nostri casi</p>';
-    $content = '';
+    $breadcrumbs = '<p><a href="index.php" lang="en">Home</a> &raquo; I nostri casi</p>';    
+    $content = '<h2>I nostri casi</h2>';
+    foreach($caseArray as $case) {
+        $content = $content.$case;
+    }
     $menu = '
         <p class="menuSection">Generale</p>
         <ul>
