eb83187e12daab4b0f23e91f763fff8c7947fbf5
LucaRomio <romioluca1999@gmail.com>
2024-01-25 14:42:42 +0100

    spostati i file nel posto giusto

--- src/400.php
+++ src/400.php
@@ -3,14 +3,14 @@
     require_once('components/sidebar.php');
 
     $layout = file_get_contents("templates/layout.html");
-    $title = 'Errore 404 | Clue Catchers';
-    $keywords = 'Clue Catchers, errore, Errore 404';
-    $description = 'Clue Catchers - Pagina di Errore 404';
+    $title = 'Errore 400 | Clue Catchers';
+    $keywords = 'Clue Catchers, errore, Errore 400';
+    $description = 'Clue Catchers - Pagina di Errore 400';
     $breadcrumbs = '<p>Errore Server</p>';
     $content = '
-    <h1 class="errorPageTitle"<span lang="en">Error 404</span></h1>
-    <h2 class="errorPageVerbose"><span lang="en">Page not found</span></h2>
-    <h3 class="errorPageJoke">Oh No! Sembra che non abbiamo piste per trovare questa pagina. Meglio ricontrollare i nostri passi</h3>
+    <h1 class="errorPageTitle"<span lang="en">Error 400</span></h1>
+    <h2 class="errorPageVerbose"><span lang="en">Bad Request</span></h2>
+    <h3 class="errorPageJoke">Oh No! Sembra che il <span lang="en">client<span> abbia formulato un a richiesta anomala</h3>
     ';
 
     $page = str_replace("[title]", $title, $layout);
--- src/500.php
+++ src/500.php
@@ -3,14 +3,14 @@
     require_once('components/sidebar.php');
 
     $layout = file_get_contents("templates/layout.html");
-    $title = 'Errore 404 | Clue Catchers';
-    $keywords = 'Clue Catchers, errore, Errore 404';
-    $description = 'Clue Catchers - Pagina di Errore 404';
-    $breadcrumbs = '<p>Errore Server</p>';
+    $title = 'Errore 500 | Clue Catchers';
+    $keywords = 'Clue Catchers, errore, Errore 500';
+    $description = 'Clue Catchers - Pagina di Errore 500';
+    $breadcrumbs = '<p><span lang="en">Errore Server</span></p>';
     $content = '
-    <h1 class="errorPageTitle"<span lang="en">Error 404</span></h1>
-    <h2 class="errorPageVerbose"><span lang="en">Page not found</span></h2>
-    <h3 class="errorPageJoke">Oh No! Sembra che non abbiamo piste per trovare questa pagina. Meglio ricontrollare i nostri passi</h3>
+    <h1 class="errorPageTitle"><span lang="en">Error 500</span></h1>
+    <h2 class="errorPageVerbose"><span lang="en">Internal Server Error</span></h2>
+    <h3 class="errorPageJoke">Oh No! Sembra che il server abbia perso le tracce della pagina! Servirà un <span lang="en">detective</span> bravo per trovarla</h3>
     ';
 
     $page = str_replace("[title]", $title, $layout);
