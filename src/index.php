140c96a8aa90757e79ca6f03280e360c3c0bf972
Andrea Mangolini <andrea.mangolini4@gmail.com>
2024-01-28 21:18:45 +0100

    Pagina home dinamica su ultimo caso

--- src/index.php
+++ src/index.php
@@ -1,29 +1,16 @@
 <?php
     require_once("app/global.php");
     require_once('components/sidebar.php');
+    require_once("data/indagine.php");
 
     $layout = file_get_contents("templates/layout.html");
     $title = 'Clue Catchers';
     $keywords = 'Clue Catchers, risolvere misteri, [CaseTitle]';
-    $description = 'Pagina iniziale del sito Clue Catchers, dove puoi provare l&apos esperienza di detective';
+    $description = 'Pagina iniziale del sito Clue Catchers, dove puoi provare l&apos;esperienza di detective';
     $breadcrumbs = '<p><span lang="en">Home</span></p>';
-    $featuredCaseTitle = 'Omicidio al Buio';
-    $featuredCaseImg = '<img src="assets/images/indagine1/capitolo0/villa.jpg" alt="Immagine di copertina del caso [CaseTitle]">';
-    $featuredCaseDesc = '<h3 class="frontPageCaseDesc">Un omicidio durante una cena al buio, in una villa lussuosa. Solo un vero <span lang="en">Clue Catcher</span> sarà in grado di risolvere il mistero...</h3>';
-    $content = '
-    <section class="frontPageContent">
-        <h2 class="frontPageTitles">Immergiti in appassionanti storie</h2>
-        <h3>Hai mai sognato di fare il <span lang="en">detective</span>? Di risolvere misteri? Hai la passione per i gialli? <span lang="en">Clue Catchers</span> è il posto giusto per voi.
-        Risolvi casi investigativi tramite semplici domande alle quali dovrai rispondere grazie agli indizi che scopri.</h3>
-    </section>
-    <section class="frontPageContent">
-        <h2 class="frontPageTitles">
-        Il nostro ultimo caso:
-        <span id="frontPageTitle">[CaseTitle]</span></h2>
-        [CaseImg]
-        [CaseDesc]
-    </section>
-    ';
+    $content = file_get_contents("templates/index_layout.html");
+    $lastIndagine = getLastIndagine();
+    $featuredCaseImg = '<img height="600" src="assets'.$lastIndagine->image_path.'" alt="">';  
 
     $page = str_replace("[title]", $title, $layout);
     $page = str_replace("[keywords]", $keywords, $page);
@@ -32,8 +19,8 @@
     $page = str_replace("[content]", $content, $page);
     $page = str_replace("[sidebar]", getSidebar("HOME"), $page);
     $page = str_replace("[userToolbar]", getUserToolBar(), $page);
-    $page = str_replace("[CaseTitle]", $featuredCaseTitle, $page);
+    $page = str_replace("[CaseTitle]", $lastIndagine->nome, $page);
     $page = str_replace("[CaseImg]", $featuredCaseImg, $page);
-    $page = str_replace("[CaseDesc]", $featuredCaseDesc, $page);
+    $page = str_replace("[CaseDesc]", $lastIndagine->descrizione, $page);
     echo $page;
 ?>
